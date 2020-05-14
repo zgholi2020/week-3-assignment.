a630248fdbcd49074f583a1193d03ce40eaa7a7f
zgholi2020 <62476910+zgholi2020@users.noreply.github.com>
2020-05-14 12:14:47 -0400

    Add files via upload

--- week 3 assignment 2-2.R
+++ week 3 assignment 2-2.R
@@ -9,3 +9,15 @@ makeCacheMatrix <- function(x = matrix()) {
   getInverse <- function() inv
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv      
+}
